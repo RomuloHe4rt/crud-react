66c6f1c773b16f39380d10ba9f73b9d863a56c02
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-31 16:34:41 -0300

    style: navigation component

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,8 +1,12 @@
 import './Nav.css'
+import NavItem from './NavItem'
 import React from 'react'
 
 // eslint-disable-next-line
 export default props =>
   <aside className="menu-area">
-    Navegação
+    <nav className="menu">
+      <NavItem href="#/" icon="home" title="Início" />
+      <NavItem href="#/users" icon="users" title="Usuários" />
+    </nav>
   </aside>
