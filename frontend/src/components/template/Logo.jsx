15a6b5c61c215fab1bafb296fca2453217e5b72e
Rômulo Silva <romulo_lss@hotmail.com>
2021-07-31 14:47:20 -0300

    style: component logo

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,8 +1,11 @@
 import './Logo.css'
+import logo from '../../assets/img/logo.png'
 import React from 'react'
 
 // eslint-disable-next-line
 export default props =>
-  <aside className="log">
-    Logo
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
   </aside>
